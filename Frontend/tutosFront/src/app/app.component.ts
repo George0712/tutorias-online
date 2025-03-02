6bb6390eea2666fd9875342779dd6141838d7a4d
George0712 <jorge12072002@gmail.com>
2025-03-02 14:17:09 -0500

    signin and signup pages added

--- Frontend/tutosFront/src/app/app.component.ts
+++ Frontend/tutosFront/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignInComponent } from './auth/features/sign-in/sign-in.component';
+import { HeaderComponent } from './shared/components/header/header.component'; // Ensure this path is correct or create the file if it doesn't exist
+import { SignUpComponent } from './auth/features/sign-up/sign-up.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, SignInComponent, SignUpComponent, FooterComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
