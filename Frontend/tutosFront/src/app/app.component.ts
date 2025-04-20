981d08b6d62ba6b0f6dcb474458aa785185ebce8
George0712 <jorge12072002@gmail.com>
2025-04-19 22:38:26 -0500

    screen student and tutor profile

--- Frontend/tutosFront/src/app/app.component.ts
+++ Frontend/tutosFront/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SignInComponent } from './auth/features/sign-in/sign-in.component';
-import { HeaderComponent } from './shared/components/header/header.component'; // Ensure this path is correct or create the file if it doesn't exist
+import { HeaderComponent } from './shared/components/header/header.component';
 import { SignUpComponent } from './auth/features/sign-up/sign-up.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { ProfileTutorComponent} from './user/tutor/Profile/profileTutor.component';
+import { ProfileStudentComponent } from './user/student/Profile/profileStudent.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, SignInComponent, SignUpComponent, FooterComponent, RouterOutlet],
+  imports: [HeaderComponent, ProfileTutorComponent, FooterComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Frontend/tutosFront/src/app/user/tutor/Profile/profileTutor.component.spec.ts
+++ Frontend/tutosFront/src/app/user/tutor/Profile/profileTutor.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProfileComponent } from './profile.component';
+import { ProfileTutorComponent } from './profileTutor.component';
 
 describe('ProfileComponent', () => {
-  let component: ProfileComponent;
-  let fixture: ComponentFixture<ProfileComponent>;
+  let component: ProfileTutorComponent;
+  let fixture: ComponentFixture<ProfileTutorComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProfileComponent]
+      imports: [ProfileTutorComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ProfileComponent);
+    fixture = TestBed.createComponent(ProfileTutorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
