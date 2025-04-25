66eb03ca7fb677a9c23dfdee9e484428a281a104
George0712 <jorge12072002@gmail.com>
2025-04-24 20:10:49 -0500

    Image added in login and DetailsTutorpage

--- Frontend/tutosFront/src/app/app.component.ts
+++ Frontend/tutosFront/src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SignInComponent } from './auth/features/sign-in/sign-in.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { SignUpComponent } from './auth/features/sign-up/sign-up.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { ProfileTutorComponent} from './user/tutor/Profile/profileTutor.component';
 import { ProfileStudentComponent } from './user/student/Profile/profileStudent.component';
+import { DetailtutorComponent } from './user/student/DetailTutor/detailtutor.component';
+import { SignInComponent } from './auth/features/sign-in/sign-in.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ProfileTutorComponent, FooterComponent, RouterOutlet],
+  imports: [HeaderComponent, SignInComponent,FooterComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
