8344b9b1f680fa83191d7ada044ed768131629ed
Omar H <omare-hernandezo@unilibre.edu.co>
2025-04-27 21:33:56 -0500

    admin vistas y retorno del rol al loguear

--- Backend/education/admin.py
+++ Backend/education/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from education.models import Education, Skill, Language
 # Register your models here.
+admin.site.register(Education)
+admin.site.register(Skill)
+admin.site.register(Language)
